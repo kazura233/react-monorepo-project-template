a90906703566ab5376878dc9a4c4c25654a8f83c
kazura233 <kazira233@gmail.com>
2022-11-01 18:06:37 +0800

    固定端口

--- packages/project-desktop/vite.config.ts
+++ packages/project-desktop/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   plugins: [eslint(), react()],
   server: {
     host: '0.0.0.0',
+    port: 3001,
     // proxy: {
     //   '/api': {
     //     target: 'http://127.0.0.1:8080',
