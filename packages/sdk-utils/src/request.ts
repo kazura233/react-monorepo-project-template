a41d253f6b6fb7cac24927fb3c0e910828fb0ad5
kazura233 <kazira233@gmail.com>
2022-11-15 17:42:23 +0800

    http相关type改为interface

--- packages/sdk-utils/src/request.ts
+++ packages/sdk-utils/src/request.ts
@@ -6,11 +6,11 @@ import axios, {
   AxiosResponse,
 } from 'axios'
 
-export type HttpStatic = AxiosStatic
-export type HttpInstance = AxiosInstance
-export type HttpRequestConfig<D = any> = AxiosRequestConfig<D>
-export type CreateHttpDefaults<D = any> = CreateAxiosDefaults<D>
-export type HttpResponse<T = any, D = any> = AxiosResponse<T, D>
+export interface HttpStatic extends AxiosStatic {}
+export interface HttpInstance extends AxiosInstance {}
+export interface HttpRequestConfig<D = any> extends AxiosRequestConfig<D> {}
+export interface CreateHttpDefaults<D = any> extends CreateAxiosDefaults<D> {}
+export interface HttpResponse<T = any, D = any> extends AxiosResponse<T, D> {}
 
 export const http: HttpStatic = axios
 
