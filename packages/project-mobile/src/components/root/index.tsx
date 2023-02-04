0840a30e597682f484079c3fcf7ab4d9d0d521a0
kazura233 <kazira233@gmail.com>
2023-02-05 04:38:26 +0800

    完善移动端国际化

--- packages/project-mobile/src/components/root/index.tsx
+++ packages/project-mobile/src/components/root/index.tsx
@@ -3,14 +3,18 @@ import { HashRouter } from 'react-router-dom'
 import { GlobalStyle } from '~/styles/global'
 import { RootRoute } from './root-router'
 import { store } from '@react-monorepo-project-template/sdk-stores'
+import { ConfigProvider } from 'antd-mobile'
+import zhCN from 'antd-mobile/es/locales/zh-CN'
 
 export const Root = () => {
   return (
-    <Provider store={store}>
-      <GlobalStyle></GlobalStyle>
-      <HashRouter>
-        <RootRoute></RootRoute>
-      </HashRouter>
-    </Provider>
+    <ConfigProvider locale={zhCN}>
+      <Provider store={store}>
+        <GlobalStyle></GlobalStyle>
+        <HashRouter>
+          <RootRoute></RootRoute>
+        </HashRouter>
+      </Provider>
+    </ConfigProvider>
   )
 }
