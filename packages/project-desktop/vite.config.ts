4bb0610be8beed0c4061c23d68482674461c01bf
kazura233 <kazira233@gmail.com>
2023-09-22 17:26:04 +0800

    eslint

--- packages/project-desktop/vite.config.ts
+++ packages/project-desktop/vite.config.ts
@@ -4,25 +4,31 @@ import eslint from 'vite-plugin-eslint'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '~': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  console.log('command：', command)
+  console.log('mode：', mode)
+
+  return {
+    base: './',
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  plugins: [eslint(), react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3001,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://127.0.0.1:8080',
-    //     changeOrigin: true,
-    //   },
-    // },
-  },
-  build: {
-    target: 'es2015',
-  },
+    plugins: [eslint(), react()],
+    server: {
+      host: '0.0.0.0',
+      port: 3001,
+      // proxy: {
+      //   '/api': {
+      //     target: 'http://127.0.0.1:8080',
+      //     changeOrigin: true,
+      //   },
+      // },
+    },
+    build: {
+      target: 'es2015',
+      cssTarget: 'chrome80',
+    },
+  }
 })
